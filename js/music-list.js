e4e9dbca69688874e17420e3eb1e6cdf88997c2c
akhil <akhilpolisetty456@gmail.com>
2022-12-24 14:24:20 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,63 @@
+let allMusic = [
+    /*
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Schor",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Ikson Anywhere - Ikson",
+        artist: "Audio Library",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef X Roy",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "music-6",
+        src: "music-6"
+    },
+    */
+    {
+        name:"Vikram Title Song",
+        artist:"Anirudh Ravichander",
+        img:"music-8",
+        src:"music-8"
+    }, {
+        name:"Once Upon A Time Song",
+        artist:"Anirudh Ravichander",
+        img:"music-7",
+        src:"music-7"
+    },
+    {
+        name:"Porata Simham Song",
+        artist:"Anirudh Ravichander",
+        img:"music-10",
+        src:"music-10"
+    },
+    {
+        name:"Kaveri Song",
+        artist:"A.R Rahman",
+        img:"music-9",
+        src:"music-9"
+    },
+]
